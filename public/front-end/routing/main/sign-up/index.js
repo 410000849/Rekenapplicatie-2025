363205d57291e66b8bdb899dd9062d0c2fdf20c4
410000849@s5.roc.a12.nl <#Superpoes7>
2025-03-26 12:04:33 +0100

    Sign up fix

--- public/front-end/routing/main/sign-up/index.js
+++ public/front-end/routing/main/sign-up/index.js
@@ -1,6 +1,5 @@
 const signup_form = document.getElementById('signup-form');
 
-signup_form.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    //
-})
+// signup_form.addEventListener('submit', async (event) => {
+//     event.preventDefault();
+// })
