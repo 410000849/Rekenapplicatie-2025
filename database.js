f0606bb2bb00aeaba69a6eb9150513417f3ea623
410000849@s5.roc.a12.nl <#Superpoes7>
2025-04-08 12:02:45 +0200

    Login cookie

--- database.js
+++ database.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import sqlite3 from 'sqlite3';
+import bcrypt from 'bcrypt';
 const db = new sqlite3.Database('database.sqlite');
 
 // DATABASE FUNCTIONS
@@ -35,8 +36,25 @@ async function createAccountNote(naam, email, hash, geboortedatum, table) {
     });
 }
 
+async function loginAccountNote(email, wachtwoord, table) {
+    return new Promise((resolve, reject) => {
+        db.get(`SELECT * FROM ${table} WHERE email = ?`, [email], async (err, row) => {
+            if (err) return reject(err);
+            if (!row) return resolve(false);
+
+            try {
+                const match = await bcrypt.compare(wachtwoord, row.wachtwoord);
+                resolve(match);
+            } catch (err) {
+                reject(err);
+            }
+        });
+    });
+}
+
 // EXPORT THE FUNCTIONS
 export {
     getAccountNote,
-    createAccountNote
+    createAccountNote,
+    loginAccountNote
 };
