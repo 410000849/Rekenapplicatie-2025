378ac291ab144eaf8812f84fada54cdb6ec74593
toot222 <150439566+toot222@users.noreply.github.com>
2025-06-18 12:12:39 +0200

    FIX GAMES

--- APIs/game.js
+++ APIs/game.js
@@ -12,12 +12,11 @@ import { getAccountNoteByCookie, voegPuntenToe } from '../database.js';
 router.post('/score', async (req, res) => {
     const cookieHeader = req.cookies['USER_TOKEN'];
     if (!cookieHeader) return res.status(400).send({ success: false, message: "No active session found" });
-    
-    const cookie = cookieHeader.split(':')[1];
+      const cookie = cookieHeader.split(':')[1];
     const { score } = req?.body;
 
     if (!cookie) return res.status(400).send({ success: false, message: "No active session found" });
-    if (!score) return res.status(400).send({ success: false, message: "Geen score gevonden" });    const accountNote = await getAccountNoteByCookie(cookie);
+    if (score === undefined || score === null) return res.status(400).send({ success: false, message: "Geen score gevonden" });const accountNote = await getAccountNoteByCookie(cookie);
     if (!accountNote || !accountNote?.id || !accountNote?.table_name || accountNote?.table_name !== 'leerling') {
         return res.status(500).send({ success: false, message: "Something went wrong" });
     }
