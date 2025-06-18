cdf0d2cabd36e3d3b738f3ee918d9e88bd30a38f
410000849@s5.roc.a12.nl <#Superpoes7>
2025-06-18 12:22:42 +0200

    89o89o8

--- APIs/game.js
+++ APIs/game.js
@@ -12,20 +12,13 @@ import { getAccountNoteByCookie, voegPuntenToe } from '../database.js';
 router.post('/score', async (req, res) => {
     const cookieHeader = req.cookies['USER_TOKEN'];
     if (!cookieHeader) return res.status(400).send({ success: false, message: "No active session found" });
-<<<<<<< HEAD
-    
+
     const cookie = cookieHeader.split(':')[1];
     const { score } = req?.body;
 
     if (!cookie) return res.status(400).send({ success: false, message: "No active session found" });
-    if (!score) return res.status(400).send({ success: false, message: "Geen score gevonden" });    const accountNote = await getAccountNoteByCookie(cookie);
-=======
-      const cookie = cookieHeader.split(':')[1];
-    const { score } = req?.body;
-
-    if (!cookie) return res.status(400).send({ success: false, message: "No active session found" });
     if (score === undefined || score === null) return res.status(400).send({ success: false, message: "Geen score gevonden" });const accountNote = await getAccountNoteByCookie(cookie);
->>>>>>> 378ac291ab144eaf8812f84fada54cdb6ec74593
+
     if (!accountNote || !accountNote?.id || !accountNote?.table_name || accountNote?.table_name !== 'leerling') {
         return res.status(500).send({ success: false, message: "Something went wrong" });
     }
